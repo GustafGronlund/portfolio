ec11a1a5dcdcb5c713e185de910cecca9fb99b54
GustafGronlund <gustafgronlund@gmail.com>
2023-03-24 12:09:31 +0100

    style: fixed hero, also did some improvements overall on the application

---
+++ src/components/ProjectsSection.svelte
@@ -0,0 +1,22 @@
+<script>
+  import ProjectCard from "../shared/ProjectCard.svelte";
+</script>
+
+<section>
+  <ProjectCard />
+  <ProjectCard />
+  <ProjectCard />
+  <ProjectCard />
+</section>
+
+<style>
+  section {
+    height: 100vh;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+  }
+</style>
---
+++ src/shared/ProjectCard.svelte
@@ -0,0 +1,24 @@
+<div class="card">
+  <div class="project-text" />
+  <div class="project-img" />
+</div>
+
+<style>
+  .card {
+    width: 80%;
+    height: 80%;
+    border-radius: 1rem;
+    background: #5d6cf9;
+    display: flex;
+    flex-direction: row;
+    padding: 2rem;
+    cursor: pointer;
+  }
+
+  .project-img {
+    height: 100%;
+    min-width: 50%;
+    background: lightyellow;
+    border-radius: 1rem;
+  }
+</style>
