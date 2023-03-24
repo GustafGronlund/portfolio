1c99c1db24039c215fe683a071628db76cc4afa1
GustafGronlund <gustafgronlund@gmail.com>
2023-03-24 09:54:56 +0100

    feat: added navigation and section for tech stack

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,81 @@
 <script>
+  import Navigation from "./components/Navigation.svelte";
+  import TechStackSection from "./components/TechStackSection.svelte";
 </script>
 
+<Navigation />
 <main>
-  <p>Dashboard</p>
+  <section class="hero">
+    <div class="hero-content">
+      <div class="text-content">
+        <h1>Gustaf Grönlund -</h1>
+        <p>Front End Developer based in Malmö</p>
+      </div>
+      <img src="../public/img/profile_picture.jpg" alt="profile_picture" />
+    </div>
+  </section>
+  <section class="about" />
+  <section class="projects" />
+  <TechStackSection />
+  <section class="contact" />
 </main>
 
 <style>
+  .hero {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .hero-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .text-content {
+    display: flex;
+    flex-direction: column;
+    padding-right: 4rem;
+  }
+
+  .text-content h1 {
+    font-size: 4rem;
+  }
+
+  .text-content p {
+    font-size: 1.5rem;
+  }
+
+  img {
+    width: 20%;
+    border-radius: 50%;
+    box-shadow: 0 0 10px 3px rgba(0, 0, 0, 0.2);
+  }
+
+  .about {
+    height: 100vh;
+    width: 100%;
+    background: beige;
+  }
+
+  .projects {
+    height: 100vh;
+    width: 100%;
+    background: lightblue;
+  }
+
+  .tech-stack {
+    height: 100vh;
+    width: 100%;
+    background: lightcoral;
+  }
+
+  .contact {
+    height: 100vh;
+    width: 100%;
+    background: lightsalmon;
+  }
 </style>
---
+++ src/components/Navigation.svelte
@@ -0,0 +1,90 @@
+<script>
+  let toggleWave = false;
+
+  const logoWave = (isWaving) => {
+    toggleWave = isWaving;
+  };
+</script>
+
+<header>
+  <span class:active={toggleWave}>👋</span>
+  <a
+    href="/app"
+    on:mouseenter={() => logoWave(true)}
+    on:mouseleave={() => logoWave(false)}
+  >
+    Gronlund.dev</a
+  >
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Projects</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    position: fixed;
+    top: 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+  }
+
+  header span {
+    padding-left: 2rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: #282828;
+    cursor: pointer;
+    font-size: 1.3em;
+    height: fit-content;
+    padding: 2rem 0 2rem 0;
+  }
+
+  nav {
+    padding: 2rem;
+    margin-left: auto;
+  }
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+    width: 20rem;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    list-style: none;
+    font-size: 1.3rem;
+    cursor: pointer;
+  }
+
+  @keyframes waving {
+    0% {
+      transform: rotate(0deg);
+    }
+    25% {
+      transform: rotate(-35deg);
+    }
+    50% {
+      transform: rotate(35deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+
+  span.active {
+    animation-name: waving !important;
+    animation-iteration-count: infinite;
+    animation-duration: 3s;
+  }
+</style>
